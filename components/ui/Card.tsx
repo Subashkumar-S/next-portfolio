4a0d9adcd711d4ae7d7158610a8406e216a766be
Subashkumar S <subashs2232@gmail.com>
2024-07-01 08:53:05 +0530

    added projects

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -4,7 +4,15 @@ import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "./3d-card";
 
-export function Card({title, description, imageSource, link}) {
+
+interface CardProps {
+  title: string;
+  description: string;
+  imageSource: string;
+  link: string;
+}
+
+export function Card({title, description, imageSource, link}: CardProps) {
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black-100 dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
